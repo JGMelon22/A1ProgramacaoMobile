d809c919f37a82938b00c4663534fdd9c9481f24
JGMelon22 <jgvasconcellos22@gmail.com>
2023-03-30 15:52:51 -0300

    Julgando se o aluno pode ou não fazer AS

--- app/src/main/java/com/example/a1_programacaomobile/SituacaoAluno.java
+++ app/src/main/java/com/example/a1_programacaomobile/SituacaoAluno.java
@@ -2,9 +2,9 @@ package com.example.a1_programacaomobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,6 +12,7 @@ public class SituacaoAluno extends AppCompatActivity {
 
     TextView textView;
     EditText txtNotaA1;
+    Button btnEnviarAS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +22,30 @@ public class SituacaoAluno extends AppCompatActivity {
         // Modifica nome da top bar
         getSupportActionBar().setTitle("A1 Programação Mobile");
 
-        // Obtem dados da activity anterior
+        // Instancia componentes gráficos
         textView = findViewById(R.id.txtViewNotaFinal2);
+        btnEnviarAS = findViewById(R.id.buttonEnviar);
+        txtNotaA1 = findViewById(R.id.editTextA1);
+
+        // Obtem dados da activity anterior
         String valor = getIntent().getStringExtra("ChaveInfoAluno");
+        String notaA1 = getIntent().getStringExtra("ChaveNotaA1Aluno");
         textView.setText(valor);
+        txtNotaA1.setText(notaA1);
 
         // Julgar se aluno precisa/pode fazer AS baseado no valor obtido da activity 1
-        txtNotaA1 = findViewById(R.id.editTextA1);
-        String notaA1 = getIntent().getStringExtra("ChaveNotaA1Aluno");
-        txtNotaA1.setText(notaA1);
+        if (Float.parseFloat(txtNotaA1.getText().toString()) > 6) {
+            btnEnviarAS.setVisibility(View.INVISIBLE);
+            txtNotaA1.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.INVISIBLE);
+        } else if (Float.parseFloat(txtNotaA1.getText().toString()) < 6 && Float.parseFloat(txtNotaA1.getText().toString()) > 4) {
+            btnEnviarAS.setVisibility(View.VISIBLE);
+            txtNotaA1.setVisibility(View.VISIBLE);
+            textView.setVisibility(View.VISIBLE);
+        } else {
+            btnEnviarAS.setVisibility(View.VISIBLE);
+            txtNotaA1.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.INVISIBLE);
+        }
     }
 }
