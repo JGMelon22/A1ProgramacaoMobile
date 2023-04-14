586e38f573a45484ef5b902fc502b765fc0162ef
rbrmafort <62165481+rbrmafort@users.noreply.github.com>
2023-04-14 02:03:50 -0300

    Emojis

--- app/src/main/java/com/example/a1_programacaomobile/AprovadoAS.java
+++ app/src/main/java/com/example/a1_programacaomobile/AprovadoAS.java
@@ -2,9 +2,15 @@ package com.example.a1_programacaomobile;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 public class AprovadoAS extends AppCompatActivity {
     TextView txtViewResultadoPosAS;
@@ -25,12 +31,55 @@ public class AprovadoAS extends AppCompatActivity {
         String nomeAluno = dados.getString("ChaveNomeAluno");
         Float valorMedia = dados.getFloat("ChaveNotaPosAS");
 
+        // initialise the layout
+        WebView webView = findViewById(R.id.webView);
+
+        // enable the javascript to load the url
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setSupportZoom(false);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // define os parâmetros da altura da WebView
+        ViewGroup.LayoutParams params = webView.getLayoutParams();
+
+
         // Julga se o Aluno passou
         if (valorMedia >= 6.0F) {
             txtViewResultadoPosAS.setText(String.format(("Parabéns, %s.\nVocê foi aprovado! \uD83E\uDD73\nSua nota: %.2f"), nomeAluno, valorMedia));
+
+            ConstraintLayout constraintLayout = findViewById(R.id.parent_layout2);
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(R.id.webView, ConstraintSet.TOP, R.id.buttonEncerrar,ConstraintSet.BOTTOM,50);
+            constraintSet.applyTo(constraintLayout);
+            params.height = 747*2;
+            params.width = 747*2;
+
+            webView.loadUrl("https://media.tenor.com/rdfk00lsVusAAAAC/thumbs-up-good-job.gif");
+
         } else {
             txtViewResultadoPosAS.setText(String.format(("Que pena, %s.\nVocê foi reprovado! \uD83D\uDE22\nSua nota: %.2f"), nomeAluno, valorMedia));
+
+            ConstraintLayout constraintLayout = findViewById(R.id.parent_layout2);
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(R.id.webView, ConstraintSet.TOP, R.id.buttonEncerrar,ConstraintSet.BOTTOM,50);
+            constraintSet.applyTo(constraintLayout);
+            params.height = 747*2;
+            params.width = 747*2;
+
+            // aplica os parâmetros atualizados na WebView
+            webView.setLayoutParams(params);
+
+            webView.loadUrl("https://media.tenor.com/N-893IGoGHsAAAAd/omg-what.gif");
         }
+
+
+
     }
 
     // Encerra o programa
--- app/src/main/java/com/example/a1_programacaomobile/SituacaoAluno.java
+++ app/src/main/java/com/example/a1_programacaomobile/SituacaoAluno.java
@@ -6,13 +6,19 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 public class SituacaoAluno extends AppCompatActivity {
 
@@ -41,23 +47,79 @@ public class SituacaoAluno extends AppCompatActivity {
         nomeAluno = dados.getString("ChaveNomeAluno");
         Float valorMedia = dados.getFloat("ChaveMedia");
 
+
+        // initialise the layout
+        WebView webView = findViewById(R.id.Wb2);
+
+        // enable the javascript to load the url
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setSupportZoom(false);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // define os parâmetros da altura da WebView
+        ViewGroup.LayoutParams params = webView.getLayoutParams();
+
+
         // Julga se o Aluno pode/deve fazer AS
         if (valorMedia >= 6.0F) {
             txtViewInfo.setText(String.format(("Parabéns, %s. Você foi aprovado! \uD83E\uDD73 Sua nota: %.2f"), nomeAluno, valorMedia));
             btnEnviarAS.setVisibility(View.INVISIBLE);
             txtNotaAS.setVisibility(View.INVISIBLE);
             txtViewAS.setVisibility(View.INVISIBLE);
+
+            ConstraintLayout constraintLayout = findViewById(R.id.parent_layout);
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(R.id.Wb2, ConstraintSet.TOP, R.id.textViewInfo,ConstraintSet.BOTTOM,200);
+            constraintSet.applyTo(constraintLayout);
+
+            params.height = 650*2;
+
+            //https://media.tenor.com/IVh7YxGaB_4AAAAC/nerd-emoji.gif
+            webView.loadUrl("https://media.tenor.com/IVh7YxGaB_4AAAAC/nerd-emoji.gif");
+
         } else if (valorMedia < 6.0F && valorMedia >= 4.0F) {
             txtViewInfo.setText(String.format(("Que pena, %s.\nVocê foi reprovado, mas poderá fazer a AS \uD83D\uDE22\nSua nota: %.2f"), nomeAluno, valorMedia));
             btnEnviarAS.setVisibility(View.VISIBLE);
             txtNotaAS.setVisibility(View.VISIBLE);
             txtViewAS.setVisibility(View.VISIBLE);
             FiltroNum.limitEditText(txtNotaAS, 0, 10, this);
+
+            // https://media.tenor.com/SBIEIe8f1MgAAAAC/emoji-emojis.gif
+            ConstraintLayout constraintLayout = findViewById(R.id.parent_layout);
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(R.id.Wb2, ConstraintSet.TOP, R.id.buttonEnviar,ConstraintSet.BOTTOM,50);
+            constraintSet.applyTo(constraintLayout);
+            params.height = 747;
+            params.width = 747;
+
+            webView.loadUrl("https://media.tenor.com/QJOxesycuHEAAAAC/chuqui-chuquiluki.gif");
+
         } else {
             txtViewInfo.setText(String.format(("Que pena, %s.\nVocê foi reprovado e não poderá fazer a AS \uD83D\uDE2D\nSua nota: %.2f"), nomeAluno, valorMedia));
             btnEnviarAS.setVisibility(View.INVISIBLE);
             txtNotaAS.setVisibility(View.INVISIBLE);
             txtViewAS.setVisibility(View.INVISIBLE);
+
+            ConstraintLayout constraintLayout = findViewById(R.id.parent_layout);
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(R.id.Wb2, ConstraintSet.TOP, R.id.textViewInfo,ConstraintSet.BOTTOM,50);
+            constraintSet.applyTo(constraintLayout);
+            params.height = 747*2;
+            params.width = 747*2;
+
+            // aplica os parâmetros atualizados na WebView
+            webView.setLayoutParams(params);
+
+            // add the url of gif
+            webView.loadUrl("https://media.tenor.com/9PTGVf4BLwYAAAAC/crying-emoji-dies.gif");
+
         }
     }
 
